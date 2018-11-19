72be2e0c45aa42acc62b9f45c6e56ad7d61d796e
Jenny-L <ms.jenny.ly@gmail.com>
2018-11-19 05:30:19 -0800

    added cities to tour and shuffled city

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <random>
+
+#include "city.hpp"
+
+using namespace std;
+
+uniform_real_distribution<double> &initaliseUniformDist();
+vector<city> shuffle_cities(vector<city>tour, mt19937 &g)
 
 int main() {
     const int CITIES_IN_TOUR = 32;
@@ -7,19 +17,47 @@ int main() {
     const int ITERATIONS = 1000;
     const int MAP_BOUNDARY = 1000;
     const int NUMBER_OF_ELITES = 1;
+    static int city_id = 0;
+
+    vector<city> tour;
+    multimap<double, vector<city>> population;
+
+    //Shuffling tours device
+    random_device rd;
+    mt19937 g(rd());
 
 
+    //Random number generator
+    uniform_real_distribution<double> unif = initaliseUniformDist();
+    default_random_engine re;
 
-    //Create a list of cities
+    //Generate a list of cites and place in tour
+    for(int i = 0; i < CITIES_IN_TOUR; ++i) {
+        double x = unif(re);
+        double y = unif(re);
+        city c{x,y, city_id};
+        ++city_id;
+        tour.push_back(c);
+    }
 
+    //Create a list of shuffle cities and put in population
+    for(int i; i < POPULATION_SIZE; ++i) {
+        shuffle_cities(tour, g);
+
+    }
+
+    //while best_distance / base_distance > improvement factor
     return 0;
 }
 
 /**
  * Shuffles a cities in a tour
  */
-void shuffle_cities() {
-
+vector<city> shuffle_cities(vector<city>tour, mt19937 &g) {
+    shuffle(tour.begin(), tour.end(), g);
+    copy(tour.begin(), tour.end(), std::ostream_iterator<int>(std::cout, " "));
+    cout << endl;
+    return tour;
 }
 
 double get_distance_between_cites() {
@@ -61,4 +99,16 @@ void mutate() {
 bool contains_city() {
 
 }
+
+/**
+ * Generates a random number between 00.000 and 1000.000
+ * @return double unif
+ */
+uniform_real_distribution<double> &initaliseUniformDist() {
+    double lowerBound = 0;
+    double upperBound = 1000;
+    uniform_real_distribution<double> unif(lowerBound, upperBound);
+    return unif;
+}
+
 
