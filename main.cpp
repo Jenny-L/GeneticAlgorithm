4b3947e1b0592fda01ad6701ff8c76341b917667
Jenny-L <ms.jenny.ly@gmail.com>
2018-11-19 13:27:46 -0800

    added tours with their fitness into a population map

--- main.cpp
+++ main.cpp
@@ -11,56 +11,6 @@
 const double SCALAR = 5;
 using namespace std;
 
-uniform_real_distribution<double> &initaliseUniformDist();
-vector<city> shuffle_cities(vector<city>tour, mt19937 &g);
-
-int main() {
-    const int CITIES_IN_TOUR = 32;
-    const int POPULATION_SIZE = 32; //number or tours in  a population
-    const int SHUFFLES = 64;
-    const int ITERATIONS = 1000;
-    const int MAP_BOUNDARY = 1000;
-    const int NUMBER_OF_ELITES = 1;
-    static int city_id = 0;
-
-    double base_distance = DBL_MIN;
-
-    vector<city> tour;
-    vector<vector<city>> population;
-
-    //Shuffling tours device
-    random_device rd;
-    mt19937 g(rd());
-
-
-    //Random number generator
-    uniform_real_distribution<double> unif = initaliseUniformDist();
-    default_random_engine re;
-
-    //Generate a list of cites and place in tour
-    for(int i = 0; i < CITIES_IN_TOUR; ++i) {
-        double x = unif(re);
-        double y = unif(re);
-        city c{x,y, city_id};
-        ++city_id;
-        tour.push_back(c);
-    }
-
-    //Shuffle the cities in the tour so it begins at a random state
-    for(int i = 0; i < SHUFFLES; ++i) {
-        shuffle_cities(tour, g);
-    }
-
-    //Create a list of shuffle cities and put in population
-    for(int i = 0; i < POPULATION_SIZE; ++i) {
-        shuffle_cities(tour, g);
-
-    }
-
-    //while best_distance / base_distance > improvement factor
-    return 0;
-}
-
 /**
  * Shuffles a cities in a tour
  */
@@ -93,13 +43,11 @@ double calculate_fitness(double distance) {
  * @param t
  * @return
  */
-double get_tour_distance(tour t) {
+double get_tour_distance(vector<city> t) {
     double distance = 0;
-    vector<city> tour = t.getContainer();
-    for(int i = 0; i < tour.size() - 1; ++i) {
-        distance = distance + get_distance_between_cites(tour[i], tour[i+1]);
+    for(int i = 0; i < t.size() - 1; ++i) {
+        distance = distance + get_distance_between_cites(t[i], t[i+1]);
     }
-    t.set_fitness_rating(calculate_fitness(distance));
     return distance;
 }
 
@@ -146,4 +94,58 @@ uniform_real_distribution<double> &initaliseUniformDist() {
     return unif;
 }
 
+
+
+
+int main() {
+    const int CITIES_IN_TOUR = 32;
+    const int POPULATION_SIZE = 32; //number or tours in  a population
+    const int SHUFFLES = 64;
+    const int ITERATIONS = 1000;
+    const int MAP_BOUNDARY = 1000;
+    const int NUMBER_OF_ELITES = 1;
+    int distance;
+
+
+    double base_distance = DBL_MIN;
+
+    vector<city> tour_container;
+    map<double, vector<city>> population;
+
+    //Shuffling tours device
+    random_device rd;
+    mt19937 g(rd());
+
+
+    //Random number generator
+    uniform_real_distribution<double> unif = initaliseUniformDist();
+    default_random_engine re;
+
+    //Generate a list of cites and place in tour_container
+    for(int i = 0; i < CITIES_IN_TOUR; ++i) {
+        int city_id = 0;
+        double x = unif(re);
+        double y = unif(re);
+        city c{x,y, city_id};
+        ++city_id;
+        tour_container.push_back(c);
+    }
+
+    //Shuffle the cities in the tour_container so it begins at a random state
+    for(int i = 0; i < SHUFFLES; ++i) {
+        shuffle_cities(tour_container, g);
+
+    }
+
+    //Create a list of shuffle cities and put in population
+    for(int i = 0; i < POPULATION_SIZE; ++i) {
+        shuffle_cities(tour_container, g);
+        double distance = get_tour_distance(tour_container);
+        double fitness = calculate_fitness(distance);
+        population.insert(pair<double, vector<city>>(fitness, tour_container));
+    }
+
+    //while best_distance / base_distance > improvement factor
+    return 0;
+}
 
